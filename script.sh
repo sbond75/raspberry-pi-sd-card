8cd81020a9c2292b706bdf2b7481964878a74e91
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-03-31 18:39:40 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Run this script using sudo. We don't use sudo in this script because we don't want it to prompt for the password midway through since operations take a while in this script.
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 set -e # stop on non-zero exit code
 set -x # verbose
 name="$1" # Something like "sift1", "fore1", etc.
@@ -10,7 +17,7 @@ fi
 partition_number=2 # Partition number of the ext4 partition on the SD card. Get from `sudo fdisk -l "$device"`
 sector_size=512 # MUST BE 512 from fdisk
 start_sector=532480 # Start sector of the ext4 partition on the SD card
-res=$(sudo resize2fs -P "$device$partition_number" | awk '{print $NF}')
+res=$(resize2fs -P "$device$partition_number" | awk '{print $NF}')
 echo "$res"
 #read asd
 size=$(($res*4096/1024/1024+1)) # in MiB
@@ -24,10 +31,10 @@ output="img_${name}_$(date "+%Y-%m-%d_%H_%M_%S_%Z")"
 read -p "go? (else ctrl-c) " asd
 # Back up partition table
 #echo 'TYPE "quit" WITHOUT QUOTES IN THIS:'
-#sudo sfdisk --backup "$device" --backup-file "$name"_partitionTable_backup
+#sfdisk --backup "$device" --backup-file "$name"_partitionTable_backup
 # Check for errors and fix them
 set +e
-sudo e2fsck -f -y -v -C 0 "$device$partition_number"
+e2fsck -f -y -v -C 0 "$device$partition_number"
 resCode="$?"
 echo "Exit code: $resCode"
 if [ "$resCode" = "0" ] || [ "$resCode" = "1" ]; then
@@ -38,12 +45,12 @@ else
 fi
 set -e
 # Resize partition's filesystem
-sudo resize2fs -p "$device$partition_number" "$size_sectors"s
-#sudo parted "$device" resize "$partition_number" "$start_sector"s "$end_sector"s
+resize2fs -p "$device$partition_number" "$size_sectors"s
+#parted "$device" resize "$partition_number" "$start_sector"s "$end_sector"s
 # Resize partition table entry
 sleep 2
 echo 'TYPE "write" WITHOUT QUOTES IN THIS:'
-echo ", $size"M | sudo sfdisk -N "$partition_number" "$device" --backup "$device" --backup-file "$name"_partitionTable_backup # M=MiB
+echo ", $size"M | sfdisk -N "$partition_number" "$device" --backup "$device" --backup-file "$name"_partitionTable_backup # M=MiB
 read -p "go?" asd
 # Make disk image
-sudo ddrescue --ask -v --size=$(($size_sectors*512)) "$device" "$output" "$output.mapfile.txt"
+ddrescue --ask -v --size=$(($size_sectors*512)) "$device" "$output" "$output.mapfile.txt"
